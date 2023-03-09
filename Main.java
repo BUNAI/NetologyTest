138a0e0f8a4829f0b26b80cfc6f910a94be0a505
Bundovich <ai.bundovich@yandex.ru>
2023-03-10 01:41:17 +0700

    Добавил инфу в Ветку

--- Main.java
+++ Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Создана ветка Now");
-        System.out.println("Now");
-        System.out.println("dwedew");
-        System.out.println("New");
+        System.out.println("Такие дела");
+
     }
 }
     
